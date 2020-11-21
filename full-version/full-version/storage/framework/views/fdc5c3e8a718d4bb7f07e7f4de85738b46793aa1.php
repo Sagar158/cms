73393fae2a6e681dede4709fe58a46f04765a89b
unknown <azamhossen01@gmail.com>
2020-11-21 18:04:21 +0600

    delete full-version

---
+++ full-version/full-version/storage/framework/views/fdc5c3e8a718d4bb7f07e7f4de85738b46793aa1.php
@@ -0,0 +1,4 @@
+<a href="https://1.envato.market/materialize_admin" target="_blank"
+  class="btn btn-buy-now gradient-45deg-indigo-purple gradient-shadow white-text tooltipped buy-now-animated tada"
+  data-position="left" data-tooltip="Buy Now!"><i class="material-icons">add_shopping_cart</i>
+</a><?php /**PATH /Users/vrushank/Downloads/materialize-admin-template-7.1/laravel-version/full-version/resources/views/pages/partials/buy-now.blade.php ENDPATH**/ ?>
