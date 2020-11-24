4e08756a54a33225e1719e5197ac9e3f94058f79
Christian Stayton <71999425+christianstayton@users.noreply.github.com>
2020-11-23 21:55:32 -0600

    footer update

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -3,11 +3,11 @@
   class="{{$configData['mainFooterClass']}} @if($configData['isFooterFixed']=== true){{'footer-fixed'}}@else {{'footer-static'}} @endif @if($configData['isFooterDark']=== true) {{'footer-dark'}} @elseif($configData['isFooterDark']=== false) {{'footer-light'}} @else {{$configData['mainFooterColor']}} @endif">
   <div class="footer-copyright">
     <div class="container">
-      <span class="left"><a style="color: white;font-weight: bold;" href="#"
-          target="_blank">POWERED BY DEKAMCO</a>
+      <span class="left"><a style="color: white;font-weight: bold;" href="https://dekam.co/"
+          target="_blank">Powered by DEKAMCO</a>
       </span>
       <span class="right hide-on-small-only" style="color: white;font-weight: bold;">
-        VERSION V1 | (C) 2020 CLEAN CREDIT CREW 
+        © 2020 Clean Credit Crew | Version 1.0.0 | <a href="https://dekam.co/cms/">Documentation | Change Log</a> 
       </span>
     </div>
   </div>
